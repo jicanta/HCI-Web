90a4261e4dd570d622a8e943e76c97615673e41c
fedemagri <fmagri@itba.edu.ar>
2024-10-03 23:58:08 -0300

    Pantalla ingresar
    Arranque la interfaz de la pantalla Ingresar.

--- HCI-Web/src/router/index.js
+++ HCI-Web/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PantallaInicio from '@/Pantallas/PantallaInicio.vue';
 import PantallaMas from '@/Pantallas/PantallaMas.vue';
+import PantallaIngresar from '@/Pantallas/PantallaIngresar.vue';
 
 const routes = [
   { path: '/', name: 'Inicio', component: PantallaInicio },
-  { path: '/more', name: 'Mas', component: PantallaMas }
+  { path: '/more', name: 'Mas', component: PantallaMas },
+  { path: '/deposit', name: 'Ingresar', component: PantallaIngresar }
 ];
 
 const router = createRouter({
