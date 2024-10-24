d0d8d3b0489db251413b77f6557e25a65bc8a306
fedemagri <fmagri@itba.edu.ar>
2024-10-23 23:40:24 -0300

    Fix.

--- HCI-Web/src/pages/HomePage.vue
+++ HCI-Web/src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <ButtonsNavBar :sections="sections"/>
 
-    <v-row class="w-100 h-100 d-flex justify-center align-center" style="margin-top: 106px;" fluid>
+    <v-row class="w-100 h-100 d-flex justify-center" style="margin-top: 106px;" fluid>
       <v-col cols="11" sm="11" md="5" lg="4" xl="4" class="d-flex flex-column align-center justify-start">
         <!-- Available balance -->
         <v-card 
