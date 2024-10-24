124b83875ffcdc48dabdf530a1ea943bcee8303c
fedemagri <fmagri@itba.edu.ar>
2024-10-23 23:39:31 -0300

    Responsive upgrade.

--- HCI-Web/src/pages/HomePage.vue
+++ HCI-Web/src/pages/HomePage.vue
@@ -1,90 +1,88 @@
 <template>
-    <ButtonsNavBar/>
+    <ButtonsNavBar :sections="sections"/>
 
-      <v-row class="w-100 h-100 d-flex justify-center" style="margin-top: 106px;" fluid>
-        <v-col cols="12" sm="12" md="5" lg="4" xl="4" class="d-flex flex-column align-center justify-start">
-          <!-- Available balance -->
-          <v-card 
-            title="Dinero disponible" 
-            class="w-100 my-4 pa-2"
-          >
-            <v-container class="d-flex flex-column align-center justify-center pa-6">
-              <!-- Money info -->
-              <v-container class="d-flex flex-row align-center pa-0">
+    <v-row class="w-100 h-100 d-flex justify-center align-center" style="margin-top: 106px;" fluid>
+      <v-col cols="11" sm="11" md="5" lg="4" xl="4" class="d-flex flex-column align-center justify-start">
+        <!-- Available balance -->
+        <v-card 
+          title="Dinero disponible" 
+          class="bg-tertiary w-100 my-4 pa-2"
+        >
+          <v-container class="d-flex flex-column align-center justify-center pa-6">
+            <!-- Money info -->
+            <v-container class="d-flex flex-row align-center pa-0">
 
-              <h1 class="font-weight-bold text-colortext2">
-                {{ showMoney ? '$' + Math.trunc(availableBalance).toLocaleString('es-ES') : '*******' }}
-              </h1>
+            <h1 class="font-weight-bold text-colortext2">
+              {{ showMoney ? '$' + Math.trunc(availableBalance).toLocaleString('es-ES') : '*******' }}
+            </h1>
 
-              <v-container class="d-flex flex-columns pa-0 ma-0 h-100">
-                <p class="text-colortext2">{{ showMoney ? getCents(availableBalance) : '**' }}</p>
-              </v-container>
-
-              <v-icon size="32" class="text-colortext2 mr-2" @click="toggleMoneyVisibility">
-                {{ showMoney ? 'mdi-eye' : 'mdi-eye-off' }}
-              </v-icon>
-
-              </v-container>
-
-              <v-divider class="my-2 w-100"/>
-
-              <!-- Actions -->
-
-              <v-col class="w-100 d-flex">
-                <v-row class="d-flex flex-row align-center justify-center w-100">
-                  <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
-                    <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-arrow-down" color="primary" elevation="4" @click="goToRoute('/deposit')" >Ingresar</v-btn>
-                    <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-pencil" color="primary"  elevation="4" @click="goToRoute('/mydata')">Tus datos</v-btn>
-                  </v-col>
-                  <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
-                    <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-cash-fast" color="primary" elevation="4">Transferir</v-btn>
-                    <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-history" color="primary" elevation="4" >Link de pago</v-btn>
-                  </v-col>
-                </v-row>
-              </v-col>
-              
+            <v-container class="d-flex flex-columns pa-0 ma-0 h-100">
+              <p class="text-colortext2">{{ showMoney ? getCents(availableBalance) : '**' }}</p>
             </v-container>
 
-          </v-card>
-          <!-- Payment methods preview -->
-          <v-card 
-            title="Medios de pago" 
-            class="w-100 my-4 pa-2"
-          >
-            <v-container class="d-flex flex-column align-center justify-center pa-6">
-              <v-container class="bg-lime-darken-4 my-1 rounded">
-                <p> •••• •••• •••• 4444 </p>
-              </v-container>
-              <v-container class="bg-shades-black my-1 rounded">
-                <p> •••• •••• •••• 4444 </p>
-              </v-container>
-              <v-container class="bg-blue-grey-lighten-3 my-1 rounded">
-                <p> •••• •••• •••• 4444 </p>
-              </v-container>
+            <v-icon size="32" class="text-colortext2 mr-2" @click="toggleMoneyVisibility">
+              {{ showMoney ? 'mdi-eye' : 'mdi-eye-off' }}
+            </v-icon>
+
+            </v-container>
+
+            <v-divider class="my-2 w-100"/>
+
+            <!-- Actions -->
+
+            <v-col class="w-100 d-flex">
+              <v-row class="d-flex flex-row align-center justify-center w-100">
+                <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
+                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-arrow-down" color="primary" elevation="4" @click="goToRoute('/deposit')" >Ingresar</v-btn>
+                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-pencil" color="primary"  elevation="4" @click="goToRoute('/mydata')">Tus datos</v-btn>
+                </v-col>
+                <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
+                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-cash-fast" color="primary" elevation="4">Transferir</v-btn>
+                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-history" color="primary" elevation="4" >Link de pago</v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
             
+          </v-container>
+
+        </v-card>
+        <!-- Payment methods preview -->
+        <v-card 
+          title="Medios de pago" 
+          class="bg-tertiary w-100 my-4 pa-2"
+        >
+          <v-container class="d-flex flex-column align-center justify-center pa-6">
+            <v-container class="bg-lime-darken-4 my-1 rounded">
+              <p> •••• •••• •••• 4444 </p>
             </v-container>
-            <v-card-actions>
-              <v-btn class="bg-primary w-100" @click="goToRoute('/payment-methods')">Ver medios de pago</v-btn>
-            </v-card-actions>
+            <v-container class="bg-shades-black my-1 rounded">
+              <p> •••• •••• •••• 4444 </p>
+            </v-container>
+            <v-container class="bg-blue-grey-lighten-3 my-1 rounded">
+              <p> •••• •••• •••• 4444 </p>
+            </v-container>
+          
+          </v-container>
+          <v-card-actions>
+            <v-btn class="bg-primary w-100" @click="goToRoute('/payment-methods')">Ver medios de pago</v-btn>
+          </v-card-actions>
 
-          </v-card>
+        </v-card>
 
-        </v-col>
+      </v-col>
 
-        <v-col cols="12" sm="12" md="5" lg="4" xl="4" class="d-flex flex-column align-center justify-start">
-          <!-- Movements preview -->
-          <v-card 
-            title="Última actividad"
-            class="w-100 my-4 pa-2"
-          >
-            <v-card-actions>
-              <v-btn class="bg-primary w-100" @click="goToRoute('/payment-methods')">Ver movimientos</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      
+      <v-col cols="11" sm="11" md="5" lg="4" xl="4" class="d-flex flex-column align-center justify-start">
+        <!-- Movements preview -->
+        <v-card 
+          title="Última actividad"
+          class="bg-tertiary w-100 my-4 pa-2"
+        >
+          <v-card-actions>
+            <v-btn class="bg-primary w-100" @click="goToRoute('/payment-methods')">Ver movimientos</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
 
 </template>
 
@@ -126,6 +124,12 @@ import { useRouter } from 'vue-router';
     router.push(route);
   };
 
+  const sections = [
+    {text: "Inicio", icon: "mdi-home", selected: true, route: "/"}, 
+    {text: "Movimientos", icon: "mdi-history", selected: false, route: "/movements"}, 
+    {text: "Medios de pago", icon: "mdi-credit-card-outline", selected: false, route: "/payment-methods"}, 
+    {text: "Invertir", icon: "mdi-cash-plus", selected: false, route: "/invest"}
+  ]
 
 </script>
 
