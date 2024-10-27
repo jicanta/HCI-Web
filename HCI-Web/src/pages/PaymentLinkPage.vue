b3ffbcf7d03d1c63e67ca1b430a79bf26cfb7a25
jicanta <ignaciocantarella@gmail.com>
2024-10-27 20:46:30 -0300

    fix style

--- HCI-Web/src/pages/PaymentLinkPage.vue
+++ HCI-Web/src/pages/PaymentLinkPage.vue
@@ -1,4 +1,4 @@
-<script setup lang="js">
+<script setup>
 import { ref } from 'vue';
 import AppDivision from '@/components/AppDivision.vue';
 import AppHeaderSecondaryScreen from '@/components/AppHeaderSecondaryScreen.vue';
