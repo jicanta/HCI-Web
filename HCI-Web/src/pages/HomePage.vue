a8d591abd55ab401fed2c3d6d49257c1b9970ecf
fedemagri <fmagri@itba.edu.ar>
2024-10-27 20:13:07 -0300

    Added deposit page.

--- HCI-Web/src/pages/HomePage.vue
+++ HCI-Web/src/pages/HomePage.vue
@@ -128,7 +128,7 @@ const maskCardNumber = (number) => {
             <v-col class="w-100 d-flex">
               <v-row class="d-flex flex-row align-center justify-center w-100">
                 <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
-                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-arrow-down" color="primary" elevation="4">Ingresar</v-btn>
+                  <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-arrow-down" color="primary" elevation="4" @click="goToRoute({ name: 'deposit' })">Ingresar</v-btn>
                   <v-btn class="text-capitalize ma-2 w-100" rounded="lg" size="x-large" width="200px" append-icon="mdi-account" color="primary"  elevation="4" @click="goToRoute({ name: 'account' })">Tus datos</v-btn>
                 </v-col>
                 <v-col cols="6" class="d-flex flex-column justify-center align-center w-100">
